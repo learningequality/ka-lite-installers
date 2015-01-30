81860ba500c44c0edcd5f10f0a7e7b23937ed2a6
Cyril Pauya <cpauya@gmail.com>
2015-01-30 19:22:19 +0800

    Setup script updates to download ka-lite and pyrun.

--- osx/setup.sh
+++ osx/setup.sh
@@ -15,9 +15,6 @@
 
 # TODO(cpauya): get the temp dir of the platform
 # TODO(cpauya): use `mktemp`
-# INSTALL_PYRUN="install-pyrun.sh"
-# PYRUN_DIR="/tmp/ka-lite-on-pyrun"
-# PYRUN="$PYRUN_DIR/bin/pyrun"
 
 # REF: http://stackoverflow.com/questions/3601515/how-to-check-if-a-variable-is-set-in-bash
 if [ -z ${TMPDIR+0} ]; then
@@ -32,22 +29,45 @@ fi
 #   exit 1
 #fi
 
-# TODO(cpauya): Since we are building a package for ka-lite on pyrun, shouldn't we put
-# this on a more accessible location, like a folder inside the script?
-WORKING_DIR="./"
+WORKING_DIR="."
 echo "Using temporary directory $WORKING_DIR..."
-PYRUN_DIR="$WORKING_DIR/ka-lite-on-pyrun"
 INSTALL_PYRUN="$WORKING_DIR/install-pyrun.sh"
+PYRUN_DIR="$WORKING_DIR/pyrun-2.7"
 PYRUN="$PYRUN_DIR/bin/pyrun"
-KA_LITE_DIR="$PYRUN_DIR/ka-lite"
+KA_LITE_ZIP="$WORKING_DIR/ka-lite.zip"
+KA_LITE_DIR="$WORKING_DIR/ka-lite"
 
 # Install PyRun
-echo "Downloading PyRun..."
-curl https://downloads.egenix.com/python/install-pyrun > $INSTALL_PYRUN
-chmod +x $INSTALL_PYRUN
-echo "Installing minimal PyRun..."
-$INSTALL_PYRUN -m --python=2.7 $PYRUN_DIR
+if [ -e "$INSTALL_PYRUN" ]; then
+    echo "Found '$INSTALL_PYRUN' so will not re-download.  Delete this file to re-download."
+else
+    echo "Downloading 'install-pyrun' script..."
+    curl https://downloads.egenix.com/python/install-pyrun > $INSTALL_PYRUN
+    chmod +x $INSTALL_PYRUN
+fi
 
-# Checkout KA-Lite
-# TODO(cpauya): git clone https://github.com/learningequality/ka-lite.git $KA_LITE_DIR
-git clone https://github.com/learningequality/ka-lite.git $KA_LITE_DIR
+if [ -d "$PYRUN_DIR" ]; then
+    echo "Found PyRun directory at '$PYRUN_DIR' so will not re-download.  Delete this directory to re-download."
+else
+    echo "Installing minimal PyRun with Python 2.7..."
+    $INSTALL_PYRUN -m --python=2.7 $PYRUN_DIR
+fi
+
+# Get KA-Lite
+if [ -e "$KA_LITE_ZIP" ]; then
+	echo "Found '$KA_LITE_ZIP' file so will not re-download.  Delete this file to re-download."
+else
+    echo "Downloading 'kalite' zip..."
+    # REF: http://stackoverflow.com/a/18222354/845481
+    # How to download source in .zip format from GitHub?
+    # TODO(cpauya): Point to the `learningequality` account when merged.
+    curl -L -o $KA_LITE_ZIP https://github.com/benjaoming/ka-lite/zipball/kalite-command/
+fi
+
+if [ -d "$KA_LITE_DIR" ]; then
+	echo "Found ka-lite directory '$KA_LITE_DIR' so will not re-download.  Delete this folder to re-download."
+else
+    echo "Extracting 'ka-lite.zip'..."
+    unzip $KA_LITE_ZIP -d $KA_LITE_DIR
+fi
+
