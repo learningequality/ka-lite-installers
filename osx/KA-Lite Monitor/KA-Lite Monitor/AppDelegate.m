312d784d51cf712ebb1827283893d9fbe6abc7d3
Cyril Pauya <cpauya@gmail.com>
2015-02-04 23:59:06 +0800

    Show user notifications of status and actions. Validate username characters, number of characters. Only run setup if no database was found.

--- osx/KA-Lite Monitor/KA-Lite Monitor/AppDelegate.m
+++ osx/KA-Lite Monitor/KA-Lite Monitor/AppDelegate.m
@@ -59,11 +59,8 @@
         NSLog(@"==> must show preferences");
         sel = @selector(showPreferences);
     }
-    [NSTimer scheduledTimerWithTimeInterval:0.1
-                                     target:self
-                                   selector:sel
-                                   userInfo:nil
-                                    repeats:NO];
+    [NSTimer scheduledTimerWithTimeInterval:0.1 target:self selector:sel userInfo:nil repeats:NO];
+    
 }
 
 
@@ -79,6 +76,7 @@
   Useful Methods
 ********************/
 
+
 int copyLocalSettings() {
     NSLog(@"==> Copying local_settings.default as local_settings.py...");
     NSString *source = [[NSBundle mainBundle] pathForResource:@"local_settings" ofType:@"default"];
@@ -90,6 +88,7 @@ int copyLocalSettings() {
     
     const char *cmd = [command UTF8String];
     int i = system(cmd);
+    showNotification(@"Copied local_settings.default to local_settings.py.");
     return i;
 }
 
@@ -158,51 +157,85 @@ void alert(NSString *message) {
 }
 
 
+void showNotification(NSString *subtitle) {
+    // REF: http://stackoverflow.com/questions/12267357/nsusernotification-with-custom-soundname?rq=1
+    // TODO(cpauya): These must be ticked by user on preferences if they want notifications, sounds, or not.
+    NSUserNotification* notification = [[NSUserNotification alloc]init];
+    notification.title = @"KA-Lite";
+    notification.subtitle = subtitle;
+    notification.soundName = @"Basso.aiff";
+//    notification.informativeText = @"informative text here";
+    [[NSUserNotificationCenter defaultUserNotificationCenter] deliverNotification:notification];
+}
+
+
+NSString *getUsernameChars() {
+    NSString *chars = @"@.+-_0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    NSLog(@"chars %@", chars);
+    return chars;
+}
+
+
+// REF: http://stackoverflow.com/a/26423271/845481
+// Check IF one String contains the same characters as another string
+- (BOOL)string:(NSString *)string containsAllCharactersInString:(NSString *)charString {
+    NSUInteger stringLen = [string length];
+    NSUInteger charStringLen = [charString length];
+    for (NSUInteger i = 0; i < charStringLen; i++) {
+        unichar c = [charString characterAtIndex:i];
+        BOOL found = NO;
+        for (NSUInteger j = 0; j < stringLen && !found; j++)
+            found = [string characterAtIndex:j] == c;
+        if (!found)
+            return NO;
+    }
+    return YES;
+}
+
+
 /********************
  END Useful Methods
  ********************/
 
 
 - (IBAction)start:(id)sender {
-    [self.statusItem setTitle:@"..."];
-    int i;
-    i = runKalite(@"start");
-    [self.statusItem setTitle:@""];
+    showNotification(@"KA-Lite is starting...");
+    int i = runKalite(@"start");
     if (i == 0) {
         [self.statusItem setImage:[NSImage imageNamed:@"stop"]];
         [self.statusItem setToolTip:@"KA-Lite is running."];
     } else {
         [self.statusItem setImage:[NSImage imageNamed:@"exclaim"]];
         [self.statusItem setToolTip:@"KA-Lite has encountered an error, pls check the Console."];
+        showNotification(@"KA-Lite has encountered an error, pls check the Console.");
     }
 }
 
 
 - (IBAction)stop:(id)sender {
     NSLog(@"==> Stopping...");
-    [self.statusItem setTitle:@"..."];
-    int i;
-    i = runKalite(@"stop");
-    [self.statusItem setTitle:@""];
+    showNotification(@"KA-Lite is stopping...");
+    int i = runKalite(@"stop");
     if (i == 0) {
         [self.statusItem setImage:[NSImage imageNamed:@"favicon"]];
-        [self.statusItem setTitle:@""];
         [self.statusItem setToolTip:@"KA-Lite is not running."];
     } else {
         [self.statusItem setImage:[NSImage imageNamed:@"exclaim"]];
         [self.statusItem setToolTip:@"KA-Lite has encountered an error, pls check the Console."];
+        showNotification(@"KA-Lite has encountered an error, pls check the Console.");
     }
 }
 
 
 - (IBAction)open:(id)sender {
     NSLog(@"==> Opening KA-Lite in browser...");
-    // TODO(cpauya): Get the ip address and port from `local_settings.py`.
+    // TODO(cpauya): Get the ip address and port from `local_settings.py` or preferences.
     // REF: http://stackoverflow.com/a/7129543/845481
     // Open URL with Safari no matter what system browser is set to
     NSURL *url = [NSURL URLWithString:@"http://127.0.0.1:8008/"];
     if( ![[NSWorkspace sharedWorkspace] openURL:url] ) {
         NSLog(@"==> Failed to open url: %@",[url description]);
+        showNotification([NSString stringWithFormat:@" Failed to open url: %@",[url description]]);
     }
 }
 
@@ -260,31 +293,32 @@ void alert(NSString *message) {
 
 - (void)savePreferences {
     /*
-     This method:
-     1. shows window for progress of setup
-     2. copy local_settings_sample.py to KALITE_DIR/kalite/local_settings.py
-     3. run `kalite manage setup --username <username> --password <password> --noinput`
-     */
-    
-    /*
-     Validate the following:
+     1. Validate the following:
         * username length max of 30 characters
         * password length max of 128 characters
         * username allowed characters are "letters, numbers and @/./+/-/_ characters" based on django.contrib.auth.models.AbstractUser
+     2. TODO(cpauya): Save the preferences: REF: http://stackoverflow.com/questions/10148788/xcode-cocoa-app-preferences
+     3. Copy local_settings_sample.py to local_settings.py
+     4. Run `kalite manage setup` if no database was found.
      */
-    NSLog(@"==> saving preferences... username %@, password %@, confirm %@",
-          self.username, self.password, self.confirmPassword);
+
+    NSLog(@"==> saving preferences...");
+    
     if (self.username == nil) {
         alert(@"Username must not be blank and can only contain letters, numbers and @/./+/-/_ characters.");
         return;
     }
 
-//    NSString *chars = @"@.+-_";
-//    // TODO(cpauya): Check for special characters in the username.
-//    if ([self.username rangeOfString:chars].location == NSNotFound) {
-//        alert(@"Invalid username characters found, please use letters, numbers and @/./+/-/_ characters.");
-//        return;
-//    }
+    NSString *usernameChars = getUsernameChars();
+    if ([self string:usernameChars containsAllCharactersInString:self.username] == NO) {
+        alert(@"Invalid username characters found, please use letters, numbers and @/./+/-/_ characters.");
+        return;
+    }
+    
+    if ([self.username length] > 30) {
+        alert(@"Username must not exceed 30 characters.");
+        return;
+    }
 
     if (self.password == nil || self.confirmPassword == nil) {
         alert(@"Invalid password or the password does not match on both fields.");
@@ -295,17 +329,31 @@ void alert(NSString *message) {
         alert(@"The password does not match on both fields.");
         return;
     }
+
+    if ([self.password length] > 128) {
+        alert(@"Password must not exceed 128 characters.");
+        return;
+    }
     
-    copyLocalSettings();
-    // TODO(cpauya): get admin account credentials from preferences
-    // How do we determine if user wants to run setup or not?
-    NSString *cmd = [NSString stringWithFormat:@"manage setup --username %@ --password %@ --noinput",
-                     self.username, self.password];
-    int i = runKalite(cmd);
-    if (i == 0) {
-        [window orderOut:[window identifier]];
-    } else {
-        alert(@"Running 'manage setup' failed, please see Console.");
+    // Copy `local_settings.default` if no `local_settings.py` was found.
+    NSString *localSettingsPath = getLocalSettingsPath();
+    if (localSettingsPath == nil) {
+        copyLocalSettings();
+    }
+    
+    // Automatically run `kalite manage setup` if no database was found.
+    NSString *databasePath = getDatabasePath();
+    if (databasePath == nil) {
+        // Get admin account credentials from preferences.
+        showNotification(@"Running `kalite manage setup`.");
+        NSString *cmd = [NSString stringWithFormat:@"manage setup --username %@ --password %@ --noinput",
+                         self.username, self.password];
+        int i = runKalite(cmd);
+        if (i == 0) {
+            [window orderOut:[window identifier]];
+        } else {
+            alert(@"Running 'manage setup' failed, please see Console.");
+        }
     }
 }
 
@@ -320,6 +368,8 @@ void alert(NSString *message) {
 - (void)loadLog {
     // REF: http://boredzo.org/blog/archives/2008-01-23/asl-searching
     // REF: http://stackoverflow.com/questions/13621343/getting-time-of-logs-stored-in-asl-log-files-with-asl-api-objective-c
+    // TODO(cpauya): Actually, it is not practical to load log data here.
+    // We must use ASL + notifications instead of this.
     return;
 }
 
