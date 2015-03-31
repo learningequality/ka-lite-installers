3d722d2cd8ead93e7623b413a7b4f1770bacf612
Cyril Pauya <cpauya@gmail.com>
2015-03-31 22:36:23 +0800

    Change to download pyrun that includes setuptools and pip instead of the minimal python only. Change the ka-lite zip file name to prevent creating a `.zip` only file. Update `setup.sh` to run `pyrun-2.7/bin/pip install -r ka-lite/requirements.txt`.

--- osx/setup.sh
+++ osx/setup.sh
@@ -6,7 +6,7 @@
 # 1. Create temporary directory
 # 2. Download the `install-pyrun` script
 # 3. Download PyRun thru `install-pyrun` script.
-# 4. Download KA-Lite zip.
+# 4. Download KA-Lite zip based on develop branch.
 # 5. Extract KA-Lite and move into `ka-lite` folder.
 # 6. Copy the extracted ka-lite and pyrun folders to the Xcode Resources folder.
 # 7. TODO(cpauya): Create the `<Xcode_Resources>/ka-lite/kalite/local_settings.py`.
@@ -22,27 +22,29 @@ if [ -z ${TMPDIR+0} ]; then
 fi
 
 STEP=1
-STEPS=6
+STEPS=7
 
 # TODO(cpauya): This works but the problem is it creates the temporary directory everytime 
 # script is run... so during devt, we will comment this for now.
 # Create temporary directory.
 echo "$STEP/$STEPS. Creating temporary directory..."
-BASE_DIR=`basename $0`
-WORKING_DIR=`mktemp -d -t ${BASE_DIR}` || exit 1
-if [ $? -ne 0 ]; then
-  echo "  $0: Can't create temp directory, exiting..."
-  exit 1
-fi
+# BASE_DIR=`basename $0`
+# WORKING_DIR=`mktemp -d -t ${BASE_DIR}` || exit 1
+# if [ $? -ne 0 ]; then
+#   echo "  $0: Can't create temp directory, exiting..."
+#   exit 1
+# fi
 
 # TODO(cpauya): Delete when done debugging.  No time to wait for downloads, let's re-use what we have.
-# WORKING_DIR="."
+WORKING_DIR="."
+
 echo "  Using temporary directory $WORKING_DIR..."
 INSTALL_PYRUN="$WORKING_DIR/install-pyrun.sh"
 PYRUN_DIR="$WORKING_DIR/pyrun-2.7"
 PYRUN="$PYRUN_DIR/bin/pyrun"
+PYRUN_PIP="$PYRUN_DIR/bin/pip"
 KA_LITE="ka-lite"
-KA_LITE_ZIP="$WORKING_DIR/$KA_LITE_.zip"
+KA_LITE_ZIP="$WORKING_DIR/$KA_LITE.zip"
 KA_LITE_DIR="$WORKING_DIR/$KA_LITE"
 KA_LITE_REPO_ZIP="https://github.com/learningequality/ka-lite/zipball/develop/"
 KA_LITE_MONITOR_RESOURCES_DIR="./KA-Lite Monitor/KA-Lite Monitor/Resources"
@@ -65,20 +67,20 @@ fi
 
 # Download PyRun.
 ((STEP++))
-echo "$STEP/$STEPS. Downloading minimal PyRun with Python 2.7..."
+echo "$STEP/$STEPS. Downloading PyRun with Python 2.7..."
 if [ -d "$PYRUN_DIR" ]; then
     echo "  Found PyRun directory at '$PYRUN_DIR' so will not re-download.  Delete this folder to re-download."
 else
-    $INSTALL_PYRUN -m --python=2.7 $PYRUN_DIR
+    $INSTALL_PYRUN --python=2.7 $PYRUN_DIR
     if [ $? -ne 0 ]; then
       echo "  $0: Can't install minimal PyRun, exiting..."
       exit 1
     fi
 fi
 
-# Get KA-Lite
+# Get KA-Lite repo
 ((STEP++))
-echo "$STEP/$STEPS. Downloading 'ka-lite' zip..."
+echo "$STEP/$STEPS. Downloading '$KA_LITE_ZIP' file from '$KA_LITE_REPO_ZIP'..."
 if [ -e "$KA_LITE_ZIP" ]; then
 	echo "  Found '$KA_LITE_ZIP' file so will not re-download.  Delete this file to re-download."
 else
@@ -106,6 +108,11 @@ else
     # Rename the extracted folder.
     mv learningequality* $KA_LITE_DIR
 fi
+
+# Run PyRun's pip install for `requirements.txt`
+((STEP++))
+echo "$STEP/$STEPS. Running 'pip install -r requirements.txt'... on '$KA_LITE_DIR' "
+$PYRUN_PIP install -r "$KA_LITE_DIR/requirements.txt"
 
 # Copy the extracted folders to the Xcode Resources folder
 ((STEP++))
