1e8eae1ca5517653204b517babed96394361d12a
Benjamin Bach <benjaoming@gmail.com>
2015-05-23 02:04:54 +0200

    More work on the build script, now builds a correct and installable debian source, need more work on kalite command and environment vars

--- debian/build.sh
+++ debian/build.sh
@@ -4,9 +4,28 @@ VERSION=$1
 SOURCE_TARBALL="ka-lite/dist/ka-lite-static-$VERSION.tar.gz"
 THIS_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-if ! which py2dsc
+if [ ! "$VERSION" ]
+then
+    echo "No version supplied, please run with version arg, e.g. ./build 1.2.3"
+    exit 1
+fi
+
+pydsc2_installed=`which py2dsc`
+if [ ! "$pydsc2_installed" ]
 then
     echo "You need to install stdeb -- sudo pip install stdeb or apt-get install python-stdeb"
+    exit 1
+fi
+
+if [ ! -h "ka-lite" ]
+then
+    echo "No source package linked in. Please symlink the ka-lite source tree to 'ka-lite'"
+    exit 1
+fi
+
+if `which deactivate`
+then
+    deactivate
 fi
 
 confirm () {
@@ -22,19 +41,27 @@ confirm () {
     esac
 }
 
-if [ ! "$VERSION" ]
-then
-    echo "No version supplied, please run with version arg, e.g. ./build 1.2.3"
-fi
-
-if [ ! -f "$SOURCE_TARBALL" ]
+if [ ! -f $SOURCE_TARBALL ]
 then
     echo "Version $VERSION does not seem to exist, do you want to build the latest source?"
     if confirm "Do you want to build it now [Y/n]"
     then
         cd ka-lite
+        
+        if [ -d ka_lite_static.egg-info ]
+        then
+            echo "Build ka-lite is more safe if you clean out all existing build files."
+            if confirm "Do you want to clean the source directory's build stuff? [Y/n]"
+            then
+                echo "Cleaning..."
+                python setup.py clean --all
+            else
+                echo "Not cleaning"
+            fi
+        fi
+        
         python setup.py sdist --static
-        if [ ! -f "$SOURCE_TARBALL" ]
+        if [ ! -f $SOURCE_TARBALL ]
         then
             echo "Built the wrong version, no file $SOURCE_TARBALL"
             exit 1
@@ -43,28 +70,29 @@ then
         echo "Doing nothing"
         exit 1
     fi
+else
+    echo "It's already built but do you need to clean everything and rebuild?"
+    if confirm "Yes, clean and rebuild [Y/n]"
+    then
+        cd ka-lite
+        python setup.py clean --all
+        python setup.py sdist --static
+    else
+        echo "Continuing build process with existing setuptools sdist"
+    fi
 fi
 
-if [ ! -h "ka-lite" ]
-then
-    echo "No source package linked in. Please symlink the ka-lite source tree to 'ka-lite'"
-    exit 1
-fi
-
-if `which deactivate`
-then
-    deactivate
-fi
-
-cd ka-lite
+cd $THIS_DIR
 
 # Build the static package
-python setup.py  --command-packages=stdeb.command sdist_dsc --static --dist-dir $THIS_DIR
+py2dsc $SOURCE_TARBALL
+
+DEBIAN_VERSION=echo "$VERSION" | sed -e 's/\([0-9]*\.[0-9]*\)\.\([.]*\)/\1\~\2/g'
 
 echo "Sources created"
 echo "You should commit relevant files to the repo, because we do track old releases. But this does not include the source code."
 echo ""
 echo "To compile debian package, run:"
 echo ""
-echo "    cd "
+echo "    cd deb_dist/ka-lite-static-$DEBIAN_VERSION"
 echo "    dpkg-buildpackage -rfakeroot -uc -us"
