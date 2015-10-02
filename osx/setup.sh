b89425f8837f085bcb3407a02cdf3d1d1756a1b4
Richard Amodia <amodia18@gmail.com>
2015-10-03 00:00:30 +0800

    Add license on dmg.

--- osx/setup.sh
+++ osx/setup.sh
@@ -68,6 +68,7 @@ RELEASE_PATH="$KA_LITE_MONITOR_PROJECT_DIR/build/Release"
 KA_LITE_LOGO_PATH="$SETUP_FILES_DIR/ka-lite-logo-full.png"
 KA_LITE_ICNS_PATH="$KA_LITE_MONITOR_DIR/Resources/images/ka-lite.icns"
 KA_LITE_README_PATH="$SETUP_FILES_DIR/README.md"
+KA_LITE_LICENSE_PATH="$SETUP_FILES_DIR/LICENSE"
 
 INSTALL_PYRUN="$WORKING_DIR/install-pyrun.sh"
 PYRUN_NAME="pyrun-2.7"
@@ -363,8 +364,13 @@ fi
 
 # Remove the .dmg if it exists.
 test -e "$DMG_PATH" && rm "$DMG_PATH"
+
 # Add the README.md to the package.
-cp -r "$SETUP_FILES_DIR" "$RELEASE_PATH" && rm -rf "$RELEASE_PATH/setup-files/ka-lite-logo-full.png"
+cp "$KA_LITE_README_PATH" "$RELEASE_PATH"
+
+# Add the LICENSE to the package.
+cp "$KA_LITE_LICENSE_PATH" "$RELEASE_PATH"
+
 # Clean-up the package.
 test -x "$RELEASE_PATH/KA-Lite-Monitor.app.dSYM" && rm -rf "$RELEASE_PATH/KA-Lite-Monitor.app.dSYM"
 
@@ -376,8 +382,10 @@ $CREATE_DMG \
     --icon "KA-Lite-Monitor.app" 150 200 \
     --app-drop-link 500 200 \
     --background "$KA_LITE_LOGO_PATH" \
-    "$DMG_PATH" \
-    "$RELEASE_PATH"
+    --eula "$RELEASE_PATH/LICENSE" \
+    "$DMG_PATH"  \
+    "$RELEASE_PATH" 
+
     # --icon-size 64 \
     # --text-size 16 \
 
