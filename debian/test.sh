07cd1b3befd2cb8639407a33621191f8bce65170
Benjamin Bach <benjaoming@gmail.com>
2015-11-04 00:06:44 +0100

    Add test script that also runs locally! w00t!!!

---
+++ debian/test.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#
+# This is a special test script that will actually install KA Lite
+# on the host system and test that stuff works!
+#
+
+set -e
+
+test_version=1.2.3
+echo "Starting tests"
+
+test_fail()
+{
+    error=$1
+    echo $error
+    exit 1
+}
+
+
+# Goto location of this script
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+
+# Create a new test package
+./make_test_pkg.sh $test_version test/ka-lite-ci-test
+cd test/ka-lite-ci-test
+
+# Build the test package
+debuild --no-lintian -us -uc
+
+# Go to the build dir
+cd ..
+
+# Create a test assessment items archive
+echo "$test_version" > assessmentitems.version
+zip test.zip assessmentitems.version
+
+# Disable asking questions
+export DEBIAN_FRONTEND=noninteractive
+
+
+# Run a test that uses a local archive
+echo "ka-lite ka-lite/download-assessment-items-url select file://$DIR/test/test.zip" | sudo debconf-set-selections
+sudo -E dpkg -i ka-lite_${test_version}_all.deb
+
+[ -f /usr/share/kalite/assessment/khan/assessmentitems.version ] || test_fail "Did not find assessment items"
