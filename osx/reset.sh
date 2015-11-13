5aaeb709b9bfdfb41c95d69b399bccd15412b977
Richard T. Amodia <amodia18@gmail.com>
2015-11-13 20:47:24 +0800

    Clean reset.sh script.

--- osx/reset.sh
+++ osx/reset.sh
@@ -5,36 +5,34 @@
 #    2. remove the .plist file, need admin
 #    3. delete the symlinked /usr/bin/kalite command, need admin
 
-KALITE_PLIST="/tmp/org.learningequality.kalite.plist"
-KALITE_PLIST_LIBRARY="/Library/LaunchAgents/org.learningequality.kalite.plist"
+KALITE_PLIST="org.learningequality.kalite.plist"
+TEMP="/tmp"
+LIBRARY="/Library/LaunchAgents"
 KALITE_EXECUTABLE_PATH="$(which kalite)"
 
 
 # Delete the .plist file.
-if [ -f $KALITE_PLIST ]
-then
-    echo "Removing $KALITE_PLIST "
-    sudo rm $KALITE_PLIST
+if [ -f "$TEMP/$KALITE_PLIST" ]; then
+    echo "Removing $TEMP/$KALITE_PLIST "
+    sudo rm "$TEMP/$KALITE_PLIST"
 else
     echo "$KALITE_PLIST not found. "
 fi
 
-if [ -f $KALITE_PLIST_LIBRARY ]
-then
-    echo "Removing $KALITE_PLIST_LIBRARY "
-    sudo rm $KALITE_PLIST_LIBRARY
+if [ -f "$LIBRARY/$KALITE_PLIST" ]; then
+    echo "Removing $LIBRARY/$KALITE_PLIST "
+    sudo rm "$LIBRARY/$KALITE_PLIST"
 else
-    echo "$KALITE_PLIST_LIBRARY not found."
+    echo "$LIBRARY/$KALITE_PLIST not found."
 fi
 
 # Delete the symlinked `kalite` executable.
 # REF: http://stackoverflow.com/questions/21799441/test-if-executable-exists-in-unix
-if which kalite > /dev/null 2>&1;
-then
+if which kalite > /dev/null 2>&1; then
     echo "Removing $KALITE_EXECUTABLE_PATH "
     sudo rm $KALITE_EXECUTABLE_PATH
 else
-    echo "kalite executable not found not found. "
+    echo "kalite executable not found. "
 fi
 
 # remove KALITE_PYTHON environment variable
