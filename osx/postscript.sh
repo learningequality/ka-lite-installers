daa35afd72d44980ffcc38b27bcd1d85c39ccd68
Danny Jim Allado <dannyallado@gmail.com>
2015-11-17 17:44:23 +0800

    Symlink kalite executable and add plist in /tmp and /Library/LaunchAgents folders.

--- osx/postscript.sh
+++ osx/postscript.sh
@@ -11,6 +11,23 @@ PYRUN_PIP="$PYRUN_DIR/bin/pip"
 BIN_PATH="$PYRUN_DIR/bin"
 ASSESSMENT_SRC="/Users/Shared/assessment/assessment.zip"
 
+SYMLINK_FILE="/Users/Shared/pyrun-2.7/bin/kalite"
+SYMLINK_TO="/usr/local/bin"
+COMMAND_SYMLINK="ln -s $SYMLINK_FILE $SYMLINK_TO"
+
+TMP="/tmp/"
+PLIST=".plist"
+ORG="org.learningequality.kalite"
+TARGET="/Library/LaunchAgents/"
+KALITE=$(which kalite)
+
+# Symlink kalite executable to /usr/local/bin
+if [ -f "$KALITE" ]; then
+    echo "$KALITE is already exist."
+else
+    $COMMAND_SYMLINK
+fi
+
 function update_env {
     echo "Updating KALITE_PYTHON environment variable"
     launchctl setenv  KALITE_PYTHON "$PYRUN"
@@ -29,6 +46,35 @@ else
 fi
 
 python $BIN_PATH/shebangcheck.py
+
+# Create plist int /tmp and /Library/LaunchAgents folders.
+if [ -f "$TMP$ORG$PLIST" ]; then
+    echo "PLIST is already exist in $TMP"
+else
+    echo "<?xml version='1.0' encoding='UTF-8'?>" >> $TMP$ORG$PLIST
+    echo "<!DOCTYPE plist PUBLIC '-//Apple//DTD PLIST 1.0//EN' 'http://www.apple.com/DTDs/PropertyList-1.0.dtd'>" >> $TMP$ORG$PLIST
+    echo "<plist version='1.0'>" >> $TMP$ORG$PLIST
+    echo "<dict>" >> $TMP$ORG$PLIST
+    echo -e "\t<key>Label</key>" >> $TMP$ORG$PLIST
+    echo -e "\t<string>org.learningequality.kalite</string>" >> $TMP$ORG$PLIST
+    echo -e "\t<key>ProgramArguments</key>" >> $TMP$ORG$PLIST
+    echo -e "\t<array>" >> $TMP$ORG$PLIST
+    echo -e "\t\t<string>sh</string>" >> $TMP$ORG$PLIST
+    echo -e "\t\t<string>-c</string>" >> $TMP$ORG$PLIST
+    echo -e "\t\t<string>launchctl setenv KALITE_PYTHON \"$TO_KALITE_PYTHON\"</string>" >> $TMP$ORG$PLIST
+    echo -e "\t</array>" >> $TMP$ORG$PLIST
+    echo -e "\t<key>RunAtLoad</key>" >> $TMP$ORG$PLIST
+    echo -e "\t<true/>" >> $TMP$ORG$PLIST
+    echo "</dict>" >> $TMP$ORG$PLIST
+    echo "</plist>" >> $TMP$ORG$PLIST
+
+    # As root, copy the .plist into /Library/LaunchAgents/
+    if [ -f "$TARGET$ORG$PLIST" ]; then
+        echo "PLIST is already exist in $TARGET"
+    else
+        sudo cp $TMP$ORG$PLIST $TARGET$ORG$PLIST
+    fi
+fi
 
 echo "Running  manage setup.."
 kalite manage setup --noinput
