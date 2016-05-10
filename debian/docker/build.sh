44aeb30d135538bea629e726e0cd3909c546f1e7
Eduard VIncent Aban <arceduardvincent@gmail.com>
2016-05-10 17:20:47 +0800

    Implement initial debian build script for docker.

---
+++ debian/docker/build.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+# **************************************************
+# Debian build script for docker
+
+# What does this script do?
+#   1. Build the docker image.
+#   2. Create a docker build tag.
+#   3. Run docker-entrypoint.sh script in the docker image.
+#   4. Display the newly built installer in the $APP_DIR.
+
+APP_DIR="/app"
+
+STEP=0
+STEPS=3
+
+((STEP++))
+echo "$STEP/$STEPS. Now building the docker image..."
+docker build .
+if [ $? -ne 0 ]; then
+    echo ".. Abort!  Error/s encountered running docker build."
+    exit 1
+fi
+
+((STEP++))
+echo "$STEP/$STEPS. Now executing docker build --tag ..."
+docker build --tag debian_build .
+if [ $? -ne 0 ]; then
+    echo ".. Abort!  Error/s encountered creating build tag ."
+    exit 1
+fi
+
+((STEP++))
+echo "$STEP/$STEPS. Now running docker-entrypoint.sh ..."
+docker run -v /app:/app -it debian_build bash /docker-entrypoint.sh
+if [ $? -ne 0 ]; then
+    echo ".. Abort!  Error/s encountered running docker-entrypoint.sh ."
+    exit 1
+fi
+
+echo "Congratulations! Your newly built installer is at '$APP_DIR'."
+docker run -v /app:/app -it debian_build ls $APP_DIR
+echo "Done!"
---
+++ debian/docker/docker-entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# **************************************************
+# Additional script that run internally in the docker image.
+#
+set -e
+
+APP_DIR="/app"
+
+STEP=0
+STEPS=1
+
+((STEP++))
+echo "$STEP/$STEPS. Now copying the *.deb files in  '$APP_DIR'..."
+cp /*.deb /app
+if [ $? -ne 0 ]; then
+    echo ".. Abort!  Error/s encountered copying the  *.deb files."
+    exit 1
+fi
