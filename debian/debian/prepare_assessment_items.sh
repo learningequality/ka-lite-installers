43bfe1ba82dc5b8c3094b338c34d60f28a351a92
Benjamin Bach <benjaoming@gmail.com>
2016-10-07 15:53:10 +0200

    Further WIP on content pack zip

--- debian/debian/prepare_assessment_items.sh
+++ debian/debian/prepare_assessment_items.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
 
-if [ ! -f debian/assessment.zip ]
+if [ ! -f debian/en.zip ]
 then
-	wget https://learningequality.org/downloads/ka-lite/0.16/content/khan_assessment.zip -O debian/assessment.zip --no-clobber
+    wget https://learningequality.org/downloads/ka-lite/0.17/content/contentpacks/en.zip -O debian/en.zip --no-clobber
 else
-	echo "Assessment.zip is already here"
+    echo "en.zip is already here"
 fi
 
-output_dir=debian/ka-lite-bundle/usr/share/kalite/assessment/khan
+output_dir=debian/ka-lite-bundle/usr/share/kalite/preseed/
 
 if [ -d $output_dir ]
 then
-	echo "Assessment items already in place, skipping"
+    echo "Content zip already in place, skipping"
 else
-	mkdir -p $output_dir
-        cd $output_dir
-        umask 022
-        cd -
-	unzip debian/assessment.zip -d $output_dir
-        chmod -R ugo+r $output_dir
+    mkdir -p $output_dir
+    cp debian/en.zip "$output_dir/contentpack-0.17.en.zip"
 fi
+
